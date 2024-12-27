6aa5392990e36fba2253f6ce5de46895df778e36
rahulap-aot <rahul.ap@aot-technologies.com>
2024-12-27 07:29:27 -0800

    ng for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DataBindingComponent } from "./components/data-binding/data-binding.component";
 import { NgIfComponent } from "./components/ng-if/ng-if.component";
+import { NgForComponent } from "./components/ng-for/ng-for.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DataBindingComponent, NgIfComponent],
+  imports: [RouterOutlet, DataBindingComponent, NgIfComponent, NgForComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/ng-for/ng-for.component.ts
+++ src/app/components/ng-for/ng-for.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ng-for',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ng-for.component.html',
   styleUrl: './ng-for.component.scss'
 })
 export class NgForComponent {
 
+  cityList: string[]= ["pune","kerala","calicut"];
+  employeeList: any[] = [
+    { empId: 1, empName: "John", empCity: "Pune" },
+    { empId: 2, empName: "Smith", empCity: "Kerala" },
+    { empId: 3, empName: "Williams", empCity: "Calicut" },
+    { empId: 4, empName: "Jones", empCity: "Kerala" },
+    { empId: 5, empName: "Brown", empCity: "Calicut" },
+    { empId: 6, empName: "Davis", empCity: "Pune" }
+  ]
 }
