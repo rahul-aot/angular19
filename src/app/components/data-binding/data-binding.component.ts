3f9f779c278fa5f44141b70a1275a8831d24c18c
rahulap-aot <rahul.ap@aot-technologies.com>
2024-12-26 23:21:37 -0800

    data binding

--- src/app/components/data-binding/data-binding.component.ts
+++ src/app/components/data-binding/data-binding.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.component.html',
   styleUrl: './data-binding.component.scss'
 })
 export class DataBindingComponent {
+
+  firstName: string = 'John';
+  rollNo: number = 1;
+  isActive: boolean = true;
+  currentDate: Date = new Date();
+  divClassName: string = 'bg-primary';
+  seletedCity: string = '';
+
+
+  constructor() {
+    console.log(this.firstName);
+  }
+
+  showWelcomeMessage(){
+    alert('Welcome  '+ this.firstName);
+  }
+
+  onCityChange(){
+    console.log('City changed');
+  }
+
 
 }
