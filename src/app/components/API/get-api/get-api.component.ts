490741d2809cf691c7664ba4cdfc5b1bf0318afc
rahulap-aot <rahul.ap@aot-technologies.com>
2024-12-31 22:15:08 -0800

    getApi completed

--- src/app/components/API/get-api/get-api.component.ts
+++ src/app/components/API/get-api/get-api.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class GetApiComponent {
 
+  users: any[] = [];
+  productList: any[] = [];
+  constructor(private http: HttpClient){
+
+  }
+
+  getUsers(){
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((res:any)=>{
+      console.log(res);
+      this.users = res
+    })
+  }
+
+  getProducts(){
+    this.http.get('https://fake-store-api.mock.beeceptor.com/api/products').subscribe((res:any)=>{
+      console.log(res);
+      this.productList = res;
+    })
+  }
 }
