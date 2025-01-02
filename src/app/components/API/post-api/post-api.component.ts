7740011267c9ae91de98b944c48fa54e0db0402f
rahulap-aot <rahul.ap@aot-technologies.com>
2025-01-01 20:47:43 -0800

    onInit

--- src/app/components/API/post-api/post-api.component.ts
+++ src/app/components/API/post-api/post-api.component.ts
@@ -1,6 +1,6 @@
 
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,7 +9,7 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './post-api.component.html',
   styleUrl: './post-api.component.scss'
 })
-export class PostApiComponent {
+export class PostApiComponent implements OnInit {
 
   http = inject(HttpClient);
   carObj: any ={
@@ -24,6 +24,15 @@ export class PostApiComponent {
   }
   carData : any[] = [];
 
+
+  ngOnInit(): void {
+    this.getAllCars();
+  }
+
+  // ngAfterViewInit(): void {
+  //   console.log("ngAfterViewInit",performance.now());
+  //   // for perfomace calculations, it willl show how much time is taken to load the html 
+  // }
   getAllCars(){
     this.http.get('https://freeapi.miniprojectideas.com/api/CarRentalApp/GetCars').subscribe((res:any)=>{
       console.log(res.data);
