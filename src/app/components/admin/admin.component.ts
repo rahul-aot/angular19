b0ca112af343f2056cc2695302b361a22519f1c4
rahulap-aot <rahul.ap@aot-technologies.com>
2025-01-02 00:39:27 -0800

    output

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ProgressBarComponent } from "../../reusable/progress-bar/progress-bar.component";
+import { TabsComponent } from "../../reusable/tabs/tabs.component";
 
 @Component({
   selector: 'app-admin',
-  imports: [RouterLink, ProgressBarComponent],
+  imports: [RouterLink, ProgressBarComponent, TabsComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
 
+  adminTabs: string[] = ["Basic Info", "Plan Info", "Payment Info"];
 }
